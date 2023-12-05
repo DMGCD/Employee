d1da1f96cebc91be9decbc26d70c6ab80e059d58
Gayan <128354648+DMGCD@users.noreply.github.com>
2023-12-05 17:56:05 +0530

    create login form and database

--- src/AppIntialize.java
+++ src/AppIntialize.java
@@ -3,12 +3,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.sql.Connection;
 
 public class AppIntialize extends Application {
+    double x;
+    double y;
 
     public static void main(String[] args) {
         launch(args);
@@ -18,7 +22,22 @@ public class AppIntialize extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent parent = FXMLLoader.load(this.getClass().getResource("view/loginForm.fxml"));
         Scene scene = new Scene(parent);
+
+        parent.setOnMousePressed((MouseEvent event)->{
+            x =event.getSceneX();
+            y =event.getSceneY();
+        });
+        parent.setOnMouseDragged((MouseEvent e)->{
+            primaryStage.setX(e.getScreenX()-x);
+            primaryStage.setY(e.getScreenY()-y);
+            primaryStage.setOpacity(.4);
+        });
+        parent.setOnMouseReleased((MouseEvent e)->{
+            primaryStage.setOpacity(1);
+        });
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setScene(scene);
+        primaryStage.show();
 
 
     }
--- src/controller/loginFormController.java
+++ src/controller/loginFormController.java
@@ -1,4 +1,96 @@
 package controller;
 
+
+import Db.Dbconnection;
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 public class loginFormController {
+    @FXML
+    private Button btnClosed;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    public void btnCloseOnAction(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+    public void txtUserNameOnAction(ActionEvent actionEvent) {
+        if(!txtUserName.getText().isEmpty()){
+            txtPassword.requestFocus();
+        }
+        else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Massege!");
+            alert.setHeaderText(null);
+            alert.setContentText("Fill the UserName");
+            alert.showAndWait();
+        }
+
+
+    }
+
+    public void txtPasswordOnAction(ActionEvent actionEvent) {
+        if(!txtPassword.getText().isEmpty()){
+            loginToEmployeeDashBoard();
+            txtUserName.clear();
+            txtPassword.clear();
+        }
+
+    }
+    public void loginToEmployeeDashBoard(){
+
+        String passworrdL = txtPassword.getText();
+        String userNameL = txtUserName.getText();
+        Connection connection = Dbconnection.getInstance().getConnection();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("select *from admin where password=? and username=?");
+            preparedStatement.setObject(1,passworrdL);
+            preparedStatement.setObject(2,userNameL);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                Parent parent = FXMLLoader.load(this.getClass().getResource("../view/dashForm.fxml"));
+                Scene scene = new Scene(parent);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.centerOnScreen();
+                stage.show();
+            }
+            else{
+                txtUserName.clear();
+                txtPassword.clear();
+                txtUserName.requestFocus();
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Information Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Wrong Password Or User Name");
+                alert.showAndWait();
+
+            }
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
